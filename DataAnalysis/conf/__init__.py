913756bf209be2c7b058153724f7e503543eab60
zhoujiebing <zhoujiebing@maimiaotech.com>
2013-05-07 13:03:55 +0800

    add conf

---
+++ DataAnalysis/conf/__init__.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Wu Liang
+@contact: garcia.wul@alibaba-inc.com
+@date: 2013-04-17 16:20
+@version: 0.0.0
+@license: Copyright alibaba-inc.com
+@copyright: Copyright alibaba-inc.com
+
+"""
+
+
+
---
+++ DataAnalysis/conf/dev/settings.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: zhoujiebin
+@contact: zhoujiebing@maimiaotech.com
+@date: 2012-12-10 17:13
+@version: 0.0.0
+@license: Copyright maimiaotech.com
+@copyright: Copyright maimiaotech.com
+
+"""
+import os
+import sys
+import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../../TaobaoOpenPythonSDK/'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../../backends/'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../../Webpage/'))
+sys.path.append(os.path.join(os.path.dirname(__file__),'../../../comm_lib/'))
+
+#北斗1 省油宝2
+SOFT_CODE_SETTING = None 
+def set_soft_code(soft_code):
+    global SOFT_CODE_SETTING
+    SOFT_CODE_SETTING = soft_code
+
+    print "soft code:", soft_code
+    from Analysis.conf import set_env
+    set_env.getEnvReady(SOFT_CODE_SETTING)
+    
+CURRENT_DIR = '/home/zhoujiebing/Analysis/DataAnalysis/'
+logger = logging.getLogger("DataAnalysis")
+hdlr = logging.FileHandler(CURRENT_DIR+'data/report_log')
+hdlr.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s-%(levelname)s: %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
+
