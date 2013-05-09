8fa5ac3788cf08b628ed26274d347696dcc35442
zhoujiebing <zhoujiebing@maimiaotech.com>
2013-05-09 17:37:31 +0800

    collect_self_order 数据获取方式由API改为db

--- DataAnalysis/analysis/analysis_self_order_script.py
+++ DataAnalysis/analysis/analysis_self_order_script.py
@@ -22,32 +22,30 @@ from CommonTools.self_order_tools import SelfOrder
 from CommonTools.logger import logger
 from CommonTools.send_tools import send_email_with_file, send_sms
 
-def send_add_order_and_support(article_code_list, date):
+def send_add_order_and_support():
     """发送每日新增订单与服务支持"""
     
     merge_order = []
     merge_support = []
-    order_head = CURRENT_DIR+'data/order/order_head.csv'
+    order_head = CURRENT_DIR+'data/order_head.csv'
     support_head = CURRENT_DIR+'data/support_head.csv'
     send_order = CURRENT_DIR+'data/send_order.csv'
     send_support = CURRENT_DIR+'data/send_support.csv'
 
-    for article_code in article_code_list:
-        merge_order.append(CURRENT_DIR+'data/order/order_'+article_code+str(date)+'.csv')
-        merge_support.append(CURRENT_DIR+'data/support_'+article_code+'.csv')
+    merge_order.append(CURRENT_DIR+'data/order.csv')
+    merge_support.append(CURRENT_DIR+'data/support.csv')
     os.system('cat %s %s > %s' % (order_head, ' '.join(merge_order), send_order))
     os.system('cat %s %s > %s' % (support_head, ' '.join(merge_support), send_support))
 
     subject = '昨日新增订单'
     content = '注意，订单抓取现在使用新脚本且近期订单API抽风的可能性比较大，请事先认真核对下再导入到百会CRM。\n有问题和不明确的请及时反馈'
     send_email_with_file('zhangfenfen@maimiaotech.com', content, subject, [send_order, send_support])
-    send_email_with_file('zhoujiebing@maimiaotech.com', content, subject, [send_order, send_support])
+    #send_email_with_file('zhoujiebing@maimiaotech.com', content, subject, [send_order, send_support])
 
 
 if __name__ == '__main__':
     try: 
-        date = datetime.date.today() - datetime.timedelta(days=1)
-        send_add_order_and_support(['ts-1796606', 'ts-1797607'], date)
+        send_add_order_and_support()
     except Exception, e:
         logger.exception('send_add_order_and_support error: %s', str(e))
         send_sms('send_add_order_and_support error:%s' % (str(e)))
--- DataAnalysis/collect/collect_self_order.py
+++ DataAnalysis/collect/collect_self_order.py
@@ -19,16 +19,12 @@ import datetime
 from CommonTools.logger import logger
 from CommonTools.send_tools import send_sms
 from DataAnalysis.conf.settings import CURRENT_DIR
-from tao_models.conf.settings import set_taobao_client
-set_taobao_client('12685542', '6599a8ba3455d0b2a043ecab96dfa6f9')
-from tao_models.vas_order_search import VasOrderSearch
 from user_center.conf.settings import WORKER_DICT
 from user_center.services.shop_db_service import ShopDBService
-
+from user_center.services.order_db_service import OrderDBService
 class UserOrder:
 
-    def __init__(self, article_code):
-        self.article_code = article_code
+    def __init__(self):
         self.code_name = {'ts-1796606':'省油宝', 'ts-1797607':'选词王', 'ts-1817244':'淘词'}
         
         self.order_type = {1:'新订', 2:'续订', 3:'升级', 4:'后台赠送', 5:'后台自动续订'}
@@ -42,42 +38,40 @@ class UserOrder:
     def get_lost_order(self, start_created, end_created):
         """获取某段时间的订单
         date 是datetime.datetime(2013,2,18,0,0,0) 这种类型"""
-
-        self.order_list = VasOrderSearch.search_vas_order(self.article_code, start_created, end_created)
-
+        self.order_list = OrderDBService.get_orders_between_time(start_created, end_created)
+    
     def get_yesterday_order(self):
         """获取昨天的订单"""
+        yesterday = datetime.date.today() - datetime.timedelta(days=1)
+        yesterday = datetime.datetime.combine(yesterday, datetime.time())
+        self.order_list = OrderDBService.get_orders_between_time(yesterday, yesterday) 
 
-        self.order_list = VasOrderSearch.search_vas_order_yesterday(self.article_code)
-
-    def write_order(self, date, lost_flag=False):
+    def write_order(self, lost_flag=False):
         """写入文件"""
 
-        self.file_obj = file(CURRENT_DIR+'data/order/order_'+self.article_code+str(date)+'.csv', 'w')
-        #服务支持没有必要存历史的记录
         if not lost_flag:
-            self.file_service_support = file(CURRENT_DIR+'data/support_'+self.article_code+'.csv', 'w')
+            self.file_obj = file(CURRENT_DIR+'data/order.csv', 'w')
+            self.file_service_support = file(CURRENT_DIR+'data/support.csv', 'w')
         else:
-            self.file_service_support = file(CURRENT_DIR+'data/lost_support_'+self.article_code+'.csv', 'a')
-        app_name = self.code_name[self.article_code] 
+            self.file_obj = file(CURRENT_DIR+'data/lost_order.csv', 'a')
+            self.file_service_support = file(CURRENT_DIR+'data/lost_support.csv', 'a')
+
         for order in self.order_list:
-            nick = order.nick
-            order_start = order.order_cycle_start
-            order_start = datetime.date(order_start.year, order_start.month, order_start.day)
-            order_end = order.order_cycle_end
-            order_end = datetime.date(order_end.year, order_end.month, order_end.day)
-            shop_search = 'http://shopsearch.taobao.com/search?q=' + nick
-            biz_type = order.biz_type
-            sale = int(order.total_pay_fee) / 100
-            shangji = nick+'_'+app_name+'_'+str(order_end)
-            self.file_obj.write('%s,%s,%s,%s,%s,%s,%d,订购使用中,每日更新,%s\n' % (nick, str(order_start),\
-                    str(order_end), app_name, shop_search, self.order_type[biz_type], sale, shangji))
+            order['app_name'] = self.code_name[order['article_code']] 
+            order_start = order['order_start']
+            order['start'] = datetime.date(order_start.year, order_start.month, order_start.day)
+            order_end = order['order_end']
+            order['end'] = datetime.date(order_end.year, order_end.month, order_end.day)
+            order['shangji'] = order['nick']+'_'+order['app_name']+'_'+str(order['end'])
+            self.file_obj.write('%(nick)s,%(start)s,%(end)s,%(app_name)s,%(order_type)s,%(sale)d,订购使用中,每日更新,%(shangji)s\n' % (order))
+            #print '%(nick)s,%(start)s,%(end)s,%(app_name)s,%(order_type)s,%(sale)d,订购使用中,每日更新,%(shangji)s' % (order)
             
             #获取专属客服id
-            shop = ShopDBService.get_or_make_shop_by_nick(nick, self.article_code, order.order_cycle_end)
+            shop = ShopDBService.get_shop_by_nick(order['nick'])
             worker_id = shop['worker_id']
             worker_name = WORKER_DICT[worker_id]
-            order_time = order.order_cycle
+
+            order_time = order['order_cycle']
             support_list = self.time_type[order_time]
             for days in support_list:
                 support = self.support_type[days]
@@ -86,9 +80,9 @@ class UserOrder:
                 if days < 0:
                     support_time = order_end-datetime.timedelta(days=-days)
                     priority = '高'
-                support_name = app_name+'_'+support+'_'+str(support_time)+'_'+str(rand)
-                self.file_service_support.write('%s,%s,%s,%s,%s,%s,%d,未回访,后台发掘\n' % \
-                        (nick, support_name, priority, app_name, support, str(support_time), rand))
+                support_name = order['app_name']+'_'+support+'_'+str(support_time)+'_'+worker_name
+                self.file_service_support.write('%s,%s,%s,%s,%s,%s,%s,未回访,后台发掘\n' % \
+                        (order['nick'], support_name, priority, order['app_name'], support, str(support_time), worker_name))
                 
         self.file_obj.close()
         self.file_service_support.close()
@@ -98,25 +92,18 @@ def collect_lost_order(start_date, end_date):
 
     while start_date < end_date:
         start_created = datetime.datetime.combine(start_date, datetime.time())
-        syb = UserOrder('ts-1796606')
-        syb.get_lost_order(start_created, start_created+datetime.timedelta(days=1))
-        syb.write_order(start_date, True)
-        xcw = UserOrder('ts-1797607')
-        xcw.get_lost_order(start_created, start_created+datetime.timedelta(days=1))
-        xcw.write_order(start_date, True)
+        order_obj = UserOrder()
+        order_obj.get_lost_order(start_created, start_created+datetime.timedelta(days=1))
+        order_obj.write_order(True)
         print '抓取 ' + str(start_date) + '的订单成功'
         start_date += datetime.timedelta(days=1)
         time.sleep(20)
 
 
 def collect_self_order_script():
-    yesterday = datetime.date.today() - datetime.timedelta(days=1)
-    syb = UserOrder('ts-1796606')
-    syb.get_yesterday_order()
-    syb.write_order(yesterday)
-    xcw = UserOrder('ts-1797607')
-    xcw.get_yesterday_order()
-    xcw.write_order(yesterday)
+    order_obj = UserOrder()
+    order_obj.get_yesterday_order()
+    order_obj.write_order()
 
 if __name__ == '__main__':
     #collect_lost_order(datetime.date(2013,4,28), datetime.date(2013,5,2))
