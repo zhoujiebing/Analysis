03bb423cc34184f3af43d0a76a7020f461e5abbb
zhoujiebing <zhoujiebing@maimiaotech.com>
2013-05-13 12:07:52 +0800

    准备上线

--- CommonTools/daily_report.py
+++ CommonTools/daily_report.py
@@ -7,7 +7,7 @@ import subprocess
 file_path = '../../'
 collect_report = os.path.join(file_path, 'DataAnalysis/collect/collect_report.py')
 analysis_report = os.path.join(file_path, 'DataAnalysis/analysis/analysis_campaign_script.py')
-collect_self_order = os.path.join(file_path, 'DataAnalysis/collect/collect_self_order.py')
+collect_update_info = os.path.join(file_path, 'DataAnalysis/collect/collect_update_info.py')
 send_self_order = os.path.join(file_path, 'DataAnalysis/send/send_self_order.py')
 
 #ps = subprocess.Popen('python %s' % (collect_report), shell=True)
@@ -16,7 +16,7 @@ send_self_order = os.path.join(file_path, 'DataAnalysis/send/send_self_order.py'
 #ps = subprocess.Popen('python %s' % (analysis_report), shell=True)
 #ps.wait()
 
-ps = subprocess.Popen('python %s' % (collect_self_order), shell=True)
+ps = subprocess.Popen('python %s' % (collect_update_info), shell=True)
 ps.wait()
 
 ps = subprocess.Popen('python %s' % (send_self_order), shell=True)
