a862e36f006166f1ccd92e7fef41a145a39369cc
zhoujiebing <zhoujiebing@maimiaotech.com>
2013-05-28 17:33:45 +0800

    小 完善

--- DataAnalysis/collect/user_report_script.py
+++ DataAnalysis/collect/user_report_script.py
@@ -23,8 +23,10 @@ def write_renew_report(file_name, nick_list):
     
     print_keys = ['nick', 'campaign', 'multi_cost', 'multi_cpc', 'multi_roi', \
             'multi_cvr', 'multi_ctr', 'count_days']
+    header_keys = ['店铺昵称', '计划名称', '多天花费', '多天cpc', '多天roi', '多天转化率',\
+            '多天点击率', '统计天数']
     write_obj = file(CURRENT_DIR+'data/renew_report.csv', 'w')
-    write_obj.write(','.join(print_keys)+'\n')
+    write_obj.write(','.join(header_keys)+'\n')
 
     for line in file(file_name):
         campaign = Report.parser_report(line)
@@ -34,6 +36,10 @@ def write_renew_report(file_name, nick_list):
         if campaign['nick'] in nick_list:
             report_str = []
             for key in print_keys:
+                if key in ['multi_cost', 'multi_cpc']:
+                    campaign[key] /= 100.0
+                elif key in ['multi_cvr', 'multi_ctr']:
+                    campaign[key] = '%.3f' % campaign[key]
                 report_str.append(str(campaign[key]))
             write_obj.write(','.join(report_str)+'\n')
     
